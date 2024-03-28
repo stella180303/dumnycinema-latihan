e1be2cb04f5e062a8918d2d2e4f2ae84a9019082
stella180303 <stelladdvnx1803@gmail.com>
2024-03-28 09:15:04 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import {getMovieList, searchMovie} from './Api';
+import {useEffect, useState} from 'react';
+import Cinema from './img/Cinema_XXI.png'
+
+const App = () =>  {
+
+
+  const [popularMovies, setPopularMovies] = useState([])
+
+  
+  useEffect(() => {
+   getMovieList().then((result) => {
+    setPopularMovies(result)
+   })
+  }, [])
+
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+          <div className="movie-wrapper" key={i}>
+            
+                <img className="movie-image"src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`} />
+                <div className='text'>
+                <div className="movie-title">{movie.title}</div>
+                <div className="movie-date">Release : {movie.release_date}</div>
+                <div className="movie-rate">Rate    : {movie.vote_average}</div>
+                </div>      
+          </div>
+      )
+    })
+  }
+  
+  const search = async (q) => {
+     if(q.length > 3){
+      const cari = await searchMovie(q)
+      setPopularMovies(cari.results)
+     }
+     
+  }
+  
+  console.log({popularMovies : popularMovies})
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="">
+      <header className="App header">
+          <div>
+            <img src={Cinema} className='logocinema' />
+
+            <input placeholder="Cari film kesayangan anda.." className="Movie-search" onChange={({target})=>search(target.value)}
+            />
+          </div>
       </header>
+
+      <div className="movie-container">
+            <PopularMovieList />
+      </div>
+        
     </div>
   );
 }
